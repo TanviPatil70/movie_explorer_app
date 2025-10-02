5a03d47391bfa2b6459b41fdef6fb1a358d84122
Tanvi Patil <tanvip2905@gmail.com>
2025-10-02 23:19:06 +0530

    fix: add types to mongodb clientPromise to fix build error

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,22 +1,31 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, MongoClientOptions } from "mongodb";
 
-const uri = process.env.MONGODB_URI; // or MONGO_URL if that's what you're using
-const options = { tls: true };
+const uri: string = process.env.MONGODB_URI || ""; // Or your connection string env variable
+const options: MongoClientOptions = {
+  tls: true,
+};
 
-let client;
-let clientPromise;
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
 
 if (!uri) {
   throw new Error("Please add your Mongo URI to .env.local");
 }
 
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient>;
+}
+
 if (process.env.NODE_ENV === "development") {
+  // In development, use global variable to preserve cache across module reloads
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
+  // In production, create a new client
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
